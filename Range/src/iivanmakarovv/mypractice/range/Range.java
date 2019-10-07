b89d850ae014c306c2f607d0ce55122396a73752
iivanmakarovv <iivanmakarovv@yandex.ru>
2019-10-07 13:17:19 +0700

    Range*

--- Range/src/iivanmakarovv/mypractice/range/Range.java
+++ Range/src/iivanmakarovv/mypractice/range/Range.java
@@ -6,10 +6,7 @@ public class Range {
 
     @Override
     public String toString() {
-        return "Range{" +
-                "from=" + from +
-                ", to=" + to +
-                '}';
+        return "{" + "from=" + from + ", to=" + to + '}';
     }
 
     public Range(double from, double to) {
@@ -42,36 +39,20 @@ public class Range {
     }
 
     public static Range intersection(Range first, Range second) {
-        if (first.from >= second.from && (first.to <= second.to && first.to >= second.from)) {
-            return new Range(first.from, first.to);
-        }
-
-        if (second.from >= first.from && (second.to <= first.to && second.to >= first.from)) {
-            return new Range(second.from, second.to);
-        }
-
-        if (first.from <= second.from && first.to >= second.from) {
-            return new Range(second.from, first.to);
-        }
-
-        if (second.from <= first.from && second.to >= first.from) {
-            return new Range(first.from, second.to);
+        if (first.from < second.to && second.from < first.to) {
+            return new Range(Math.max(first.from, second.from), Math.min(first.to, second.to));
         }
 
         return null;
     }
 
     public static Range[] union(Range first, Range second) {
-        if (first.from <= second.from && (first.to <= second.to && second.from <= first.to)) {
-            return new Range[]{new Range(first.from, second.to)};
-        }
-
-        if (second.from <= first.from && (second.to <= first.to && first.from <= second.to)) {
-            return new Range[]{new Range(second.from, first.to)};
+        if (first.from <= second.to && second.from <= first.to) {
+            return new Range[]{new Range(Math.min(first.from, second.from), Math.max(first.to, second.to))};
         }
 
         if (first.to < second.from) {
-            return new Range[]{new Range(first.from, first.to),new Range(second.from, second.to)};
+            return new Range[]{new Range(first.from, first.to), new Range(second.from, second.to)};
         }
 
         return new Range[]{new Range(second.from, second.to), new Range(first.from, first.to)};
@@ -87,10 +68,14 @@ public class Range {
         }
 
         if (first.from < second.from && first.to > second.to) {
-            return new Range[]{new Range(first.from, second.from),new Range(second.to, first.to)};
+            return new Range[]{new Range(first.from, second.from), new Range(second.to, first.to)};
         }
 
-        return new Range[]{new Range(0, 0)};
+        if (first.from >= second.from && first.to <= second.to) {
+            return null;
+        }
+
+        return new Range[]{new Range(first.from, first.to)};
     }
 }
 
--- Range/src/iivanmakarovv/mypractice/rangemain/Main.java
+++ Range/src/iivanmakarovv/mypractice/rangemain/Main.java
@@ -1,25 +1,38 @@
 package iivanmakarovv.mypractice.rangemain;
 
 import iivanmakarovv.mypractice.range.Range;
+
 import static iivanmakarovv.mypractice.range.Range.*;
 
 public class Main {
     public static void main(String[] args) {
-        Range first = new Range(1, 50);
-        Range second = new Range(50, 150);
+        Range first = new Range(10, 15);
+        Range second = new Range(1, 5);
 
         Range resultIntersection = intersection(first, second);
         Range[] resultUnion = union(first, second);
         Range[] resultDifference = difference(first, second);
 
-        System.out.println(resultIntersection.toString());
+        System.out.println("Пересечение:");
+        try {
+            System.out.println(resultIntersection.toString());
+        } catch (NullPointerException e) {
+            System.out.println("Пересечений нет");
+        }
 
-        /*for (Object o : resultUnion) {
+        System.out.println("Объединение:");
+        for (Object o : resultUnion) {
             System.out.println(o.toString());
-        }*/
+        }
 
-        /*for (Object o : resultDifference) {
-            System.out.println(o.toString());
-        }*/
+        System.out.println("Разность:");
+        try {
+            for (Object o : resultDifference) {
+                System.out.println(o.toString());
+            }
+        } catch (NullPointerException e) {
+            System.out.println("Разности нет");
+        }
+
     }
 }
