0996b52f31d8c72cf5b4e90878d4de7f829f1eb6
iivanmakarovv <iivanmakarovv@yandex.ru>
2019-10-11 14:05:33 +0700

    Релиз

--- Vector/src/iivanmakarovv/mypractice/vector/Vector.java
+++ Vector/src/iivanmakarovv/mypractice/vector/Vector.java
@@ -1,34 +1,130 @@
 package iivanmakarovv.mypractice.vector;
 
+import java.util.Arrays;
+
 public class Vector {
     private double n;
+    private double[] array;
 
     public Vector(double n) {
-        double[] vector = new double[(int) n];
+        if (n <= 0) {
+            throw new IllegalArgumentException("n должн быть больше 0");
+        }
+        this.n = n;
+        this.array = new double[(int) n];
         for (int i = 0; i < n; ++i) {
-            vector[i] = 0;
+            this.array[i] = 0;
         }
     }
 
-    public Vector(Vector v) {
-        n = v.n;
+    public Vector(Vector vector) {
+        this.n = vector.n;
+        this.array = Arrays.copyOf(vector.array, (int) this.n);
     }
 
     public Vector(double[] array) {
-        double[] vector = new double[array.length];
-        for (int i = 0; i < array.length; ++i) {
-            vector[i] = array[i];
+        if (array.length <= 0) {
+            throw new IllegalArgumentException("n должн быть больше 0");
         }
+        this.n = array.length;
+        this.array = Arrays.copyOf(array, array.length);
     }
 
     public Vector(double n, double[] array) {
-        double[] vector = new double[(int)n];
-        for (int i = 0; i < array.length; ++i) {
-            vector[i] = array[i];
+        if (n <= 0) {
+            throw new IllegalArgumentException("n должн быть больше 0");
         }
-        for (int i = array.length; i < n; ++i) {
-            vector[i] = 0;
-        }
+        this.n = n;
+        this.array = Arrays.copyOf(array, (int) n);
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Vector vector = (Vector) o;
+
+        if (this.n != vector.n) return false;
+        for (int i = 0; i < this.n; ++i) {
+            if (this.array[i] != vector.array[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 37;
+        int hash = 1;
+        for (int i = 0; i < this.n; ++i) {
+            hash = prime * hash + Double.hashCode(this.array[i]);
+        }
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(array);
+    }
+
+    public void sum(Vector vector) {
+        if (this.array.length >= vector.array.length) {
+            for (int i = 0; i < vector.array.length; ++i) {
+                this.array[i] += vector.array[i];
+            }
+        } else {
+            for (int i = 0; i < this.array.length; ++i) {
+                vector.array[i] += this.array[i];
+            }
+            this.array = vector.array;
+        }
+    }
+
+    public void difference(Vector vector) {
+        if (this.array.length >= vector.array.length) {
+            for (int i = 0; i < vector.array.length; ++i) {
+                this.array[i] -= vector.array[i];
+            }
+        } else {
+            for (int i = 0; i < this.array.length; ++i) {
+                vector.array[i] -= this.array[i];
+                vector.array[i] *= -1;
+            }
+            this.array = vector.array;
+        }
+    }
+
+    public void scalarMultiplication(double scalar) {
+        for (int i = 0; i < this.array.length; ++i) {
+            this.array[i] *= scalar;
+        }
+    }
+
+    public void reversVector() {
+        for (int i = 0; i < this.array.length; ++i) {
+            this.array[i] *= -1;
+        }
+    }
+
+    public double getVectorLength() {
+        double sum = 0;
+        for (double i : this.array) {
+            sum += i * i;
+        }
+        return Math.sqrt(sum);
+    }
+
+    public double getComponent(int i) {
+        return this.array[i];
+    }
+
+    public void setComponent(int i, double component) {
+        this.array[i] = component;
+    }
+
+    public double getSize() {
+        return this.n;
     }
 }
--- Vector/src/iivanmakarovv/mypractice/vectormain/Main.java
+++ Vector/src/iivanmakarovv/mypractice/vectormain/Main.java
@@ -3,11 +3,41 @@ package iivanmakarovv.mypractice.vectormain;
 import iivanmakarovv.mypractice.vector.Vector;
 
 public class Main {
+    private static Vector getVectorsSum(Vector first, Vector second) {
+        first.sum(second);
+        return new Vector(first);
+    }
+
+    private static Vector getVectorsDifference(Vector first, Vector second) {
+        first.difference(second);
+        return new Vector(first);
+    }
+
+    private static double getVectorsScalarMultiplication(Vector first, Vector second) {
+        double scalarMulti = 0;
+        for (int i = 0; i < Math.min(first.getSize(), second.getSize()); ++i) {
+            scalarMulti += first.getComponent(i) * second.getComponent(i);
+        }
+        return scalarMulti;
+    }
+
+
     public static void main(String[] args) {
-        double[] array = new double[]{1,2,3,4,5};
-        Vector vector1 = new Vector(5);
-        Vector vector2 = new Vector(5);
-        Vector vector3 = new Vector(5);
-        Vector vector4 = new Vector(5);
+        double[] array = {1, 2, 3, 4, 5};
+
+        Vector vector1 = new Vector(6);
+        Vector vector2 = new Vector(array);
+        Vector vector3 = new Vector(6, array);
+        Vector vector4 = new Vector(vector3);
+
+        System.out.println(vector1.toString());
+
+        Vector vector5 = getVectorsSum(vector3, vector2);
+        System.out.println(vector5.toString());
+
+        Vector vector6 = getVectorsDifference(vector5, vector2);
+        System.out.println(vector6.toString());
+
+        System.out.println(getVectorsScalarMultiplication(vector6, vector4));
     }
 }
