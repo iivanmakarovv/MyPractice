50190a9e587d775e70db8b9b658c312a895d4a29
iivanmakarovv <iivanmakarovv@yandex.ru>
2019-11-12 15:51:53 +0700

    Добавил break; в третьем методе, чтобы оптимизировать работу метода.

--- ArrayListHome/src/iivanmakarovvarraylisthome/arraylisthomemain/ArrayListHome.java
+++ ArrayListHome/src/iivanmakarovvarraylisthome/arraylisthomemain/ArrayListHome.java
@@ -42,6 +42,7 @@ public class ArrayListHome {
             for (Integer element : list2) {
                 if (list.get(i).equals(element)) {
                     isRepeating = true;
+                    break;
                 }
             }
 
